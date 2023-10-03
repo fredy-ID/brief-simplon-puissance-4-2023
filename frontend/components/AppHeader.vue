d8cfcb8ec9023b5a83b91aba796c6ba1b9245b9f
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2023-10-03 20:04:06 +0400

    nettoyage interface

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -3,18 +3,7 @@
         <nav>
           <div class="navbar">
             <div class="navbar-start">
-              <div class="dropdown">
-                <label tabindex="0" class="btn btn-ghost lg:hidden">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                  <li><a href="#about">A propos</a></li>
-                  <li><a href="#skills">Compétences</a></li>
-                  <li><a href="#portfolio">Portfolio</a></li>
-                  <li><a href="#contact">Contact</a></li>
-                </ul>
-              </div>
-              <a class="btn btn-ghost normal-case text-xl">Accueil</a>
+              <a class="btn btn-ghost normal-case text-xl">Projet puissance 4 🔥</a>
             </div>
             <div class="navbar-center hidden lg:flex">
               <ul class="menu menu-horizontal px-1">
@@ -23,9 +12,6 @@
                 <li><a href="#portfolio">Portfolio</a></li>
                 <li><a href="#contact">Contact</a></li>
               </ul>
-            </div>
-            <div class="navbar-end">
-              <a class="btn">Chatbot</a>
             </div>
           </div>
         </nav>
