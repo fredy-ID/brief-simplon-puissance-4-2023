1df12e1ad152c20a19e1e7e444ea793b208da887
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2023-10-03 15:14:08 +0400

    Update Grid.vue

--- frontend/components/Grid.vue
+++ frontend/components/Grid.vue
@@ -1,21 +1,21 @@
 <template>
     <div>
-      <div class="board h-96 w-96">
-        <div class="grid relative h-full">
-          <div v-for="(row, rowIndex) in grid" :key="rowIndex" class="row border border-2 border-neutral h-full">
-            <div v-for="(cell, colIndex) in row" :key="colIndex" class="col border border-2 border-warning h-full w-100">
-                <div class="cell h-full">text</div>
+      <div class="relative board h-96 w-96">
+        <div class="absolute z-0 c-grid flex h-full w-full"></div>
+        <div class="h-full w-full flex flex-col c-cells">
+            <div v-for="(col, rowIndex) in grid" :key="rowIndex" class="flex flew-row grow border border2 border-error">
+                <div v-for="(row, colIndex) in grid[0]" :key="colIndex" class="grow border border2 border-warning">
+                    {{ rowIndex }} - {{ colIndex }}
+                </div>
             </div>
-          </div>
         </div>
+        
       </div>
     </div>
   </template>
   
 <script setup lang="ts">
     import { ref, Ref } from 'vue';
-    const columns: Ref<number> = ref(7);
-    const rows: Ref<number> = ref(6);
 
     const grid: Ref<string[][]> = ref([
         ["E", "E", "E", "E", "E", "E", "E"],
@@ -26,17 +26,23 @@
         ["E", "E", "E", "E", "E", "E", "E"]
     ]);
 
+    const columns: Ref<number> = ref(grid.value[0].length);
+    const rows: Ref<number> = ref(grid.value.length);
+
 </script>
   
 <style scoped>
-.grid {
+.c-grid, .c-cells {
+    border: 15px solid #2a5782;
+}
+.c-grid {
     display: grid;
     grid-template-columns: repeat(v-bind('columns'), 1fr);
     grid-template-rows: repeat(v-bind('rows'), 1fr);
     aspect-ratio: v-bind('columns') / v-bind('rows');
     align-items: center;
     justify-content: center;
-    border: 15px solid #2a5782;
+    
     background-color: #336dac;
     background: radial-gradient(
         circle, 
@@ -47,7 +53,12 @@
         #2a5782 80%
     );
     background-size: calc(100% / v-bind('columns')) calc(100% / v-bind('rows'));
+    z-index: -1;
+    
 }
 
+.fff{
+    
+}
 
 </style>
