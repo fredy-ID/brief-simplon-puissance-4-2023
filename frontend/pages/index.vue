a44a25ac8276331e80df9b5f299eac3daef8c28a
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2023-10-03 23:07:47 +0400

    jeu en local complété

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -38,7 +38,7 @@ players.value = [
 currentPlayer.value = players.value[0]
 
 
-function playTurn(hasPlayedPlayer: Players) {
+function hasPlayed(hasPlayedPlayer: Players) {
     if (gameSelection.value) {
         return;
     }
@@ -46,18 +46,76 @@ function playTurn(hasPlayedPlayer: Players) {
         return;
     }
     if(players && players.value) {
-        return currentPlayer.value = players.value.find(p => p.color !== hasPlayedPlayer.color);
+        return players.value.find(p => p.color !== hasPlayedPlayer.color);
     }
 }
 
-function gameEvent(stateOfPlay: {color: string, row: number, col: number}) {
-    console.log('stateOfPlay: ', stateOfPlay)
-    grid.value[stateOfPlay.row][stateOfPlay.col] = stateOfPlay.color;
-    if(currentPlayer && currentPlayer.value) {
-        currentPlayer.value = playTurn(currentPlayer.value)
-        
+function checkVictory(grid: string[][], row: number, col: number, player: string): boolean {
+    // Vérification en hauteur
+    for (let i = 0; i <= grid.length - 4; i++) {
+        if (
+            grid[i][col] === player &&
+            grid[i + 1][col] === player &&
+            grid[i + 2][col] === player &&
+            grid[i + 3][col] === player
+        ) {
+            return true;
+        }
     }
 
+    // Vérification en largeur
+    for (let j = 0; j <= grid[row].length - 4; j++) {
+        if (
+            grid[row][j] === player &&
+            grid[row][j + 1] === player &&
+            grid[row][j + 2] === player &&
+            grid[row][j + 3] === player
+        ) {
+            return true;
+        }
+    }
+
+    // Vérification en diagonale (bas gauche vers haut droite)
+    for (let i = 3; i < grid.length; i++) {
+        for (let j = 0; j <= grid[i].length - 4; j++) {
+            if (
+                grid[i][j] === player &&
+                grid[i - 1][j + 1] === player &&
+                grid[i - 2][j + 2] === player &&
+                grid[i - 3][j + 3] === player
+            ) {
+                return true;
+            }
+        }
+    }
+
+    // Vérification en diagonale (haut gauche vers bas droite)
+    for (let i = 0; i <= grid.length - 4; i++) {
+        for (let j = 0; j <= grid[i].length - 4; j++) {
+            if (
+                grid[i][j] === player &&
+                grid[i + 1][j + 1] === player &&
+                grid[i + 2][j + 2] === player &&
+                grid[i + 3][j + 3] === player
+            ) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function gameEvent(stateOfPlay: { color: string; row: number; col: number }) {
+    console.log('stateOfPlay: ', stateOfPlay);
+    grid.value[stateOfPlay.row][stateOfPlay.col] = stateOfPlay.color;
+    if (checkVictory(grid.value, stateOfPlay.row, stateOfPlay.col, stateOfPlay.color)) {
+        console.log(`Le joueur ${stateOfPlay.color} a gagné !`);
+    } else {
+        if (currentPlayer && currentPlayer.value) {
+            currentPlayer.value = hasPlayed(currentPlayer.value);
+        }
+    }
 }
 
 </script>
