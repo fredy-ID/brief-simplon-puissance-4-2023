1f0068dc512b67d0a33d41658dc57b8828c1b6cf
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2023-10-05 07:40:13 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
+        origin: "*"
     },
 });
 
